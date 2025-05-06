1f200a4b3035ed00fd5818a0520d3ecef22ac257
Harini <nharinirmd@gmail.com>
2025-05-06 22:20:48 +0200

    Updated home page

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <h1 className="text-5xl my-20 mx-4">Welcome to Game</h1>
+    </div>
+  );
+};
+
+export default home;
