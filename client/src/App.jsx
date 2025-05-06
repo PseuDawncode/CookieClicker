7a66a50dbaecd8dfd45390fc5eb9a84bd20c8d77
Harini <nharinirmd@gmail.com>
2025-05-06 22:19:46 +0200

    Basic routing updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
-
-import "./App.css";
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Nav from "./components/Nav";
+import Home from "./pages/home";
 function App() {
   return (
     <>
-      <div className="flex flex-row justify-center items-center">
-        Cookie Clicker
-      </div>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}>
+            {" "}
+            Home{" "}
+          </Route>
+          <Route path="/login" element={<Login />}>
+            {" "}
+            Login{" "}
+          </Route>
+          <Route path="/register" element={<Register />}>
+            {" "}
+            Register{" "}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
