c8ae4e9a3146fc135a530a5f39f62da502dec54f
000henrik@gmail.com <000henrik@gmail.com>
2025-05-20 11:54:37 +0200

    Test

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useState } from "react";
+import CookieGame from "../components/cookieGame";
 
 const home = () => {
   return (
     <div className="flex flex-row justify-center items-center px-4">
       <h1 className="sm:text-3xl md:text-5xl my-10 mx-4 text-center">
-        Welcome to Game
+       <CookieGame />
       </h1>
     </div>
   );
